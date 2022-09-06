b02ecf90f172a3df65a72e33d1459e8d607bb184
Stanislav <s.boyko@robo.cash>
2022-09-06 22:32:43 +0300

    lesson 9 - oauth, event, listener, vk authorization

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -3,6 +3,10 @@
     <div class="offset-2">
     <h2>Добро пожаловать, {{ Auth::user()->name }}</h2>
     <br>
+    @if(Auth::user()->avatar)
+        <img src="{{ Auth::user()->avatar }}" style="width:200px;">
+    @endif
+        <br><br>
     @if(Auth::user()->is_admin === true)
        <a href="{{ route('admin.index') }}">В админку</a>
     @endif
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
 
 use App\Http\Controllers\Account\IndexController as AccountController;
+use App\Http\Controllers\Admin\ParserController;
 
 use App\Http\Controllers\Admin\IndexController as AdminController;
 use App\Http\Controllers\Admin\NewsController as AdminNewsController;
 use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+
+use App\Http\Controllers\SocialProvidersController;
 
 
 
@@ -34,6 +37,7 @@ Route::middleware('auth')->group(function() {
     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function() {
 	Route::get('/', AdminController::class)
 		->name('index');
+    Route::get('/parser', ParserController::class)->name('parser');
 	Route::resource('categories', AdminCategoryController::class);
 	Route::resource('news', AdminNewsController::class);
 });
@@ -69,3 +73,12 @@ Route::get('/sessions', function() {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'guest'], function() {
+    Route::get('/auth/redirect/{driver}', [SocialProvidersController::class, 'redirect'])
+        ->where('driver', '\w+')
+        ->name('social.auth.redirect');
+
+    Route::get('/auth/callback/{driver}', [SocialProvidersController::class, 'callback'])
+        ->where('driver', '\w+');
+});
